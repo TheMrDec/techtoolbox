23463e34a5c75ecc8c7cac54cc58b2aa5e15d7fb
TheMrDec <prion.awesome@gmail.com>
2023-03-17 11:56:32 -0500

    module loader added
    Spent too long learning how to recurse through a directory to find and load modules.

---
+++ ToolBox.ps1
@@ -0,0 +1,13 @@
+$rundir = Get-Location
+$modules = Get-Childitem -Path $rundir\modules -Filter *.psm1 -Recurse -EA SilentlyContinue -Force
+
+ForEach ($module in $modules) {
+    Write-Host $module.FullName
+    Import-Module $module.FullName
+}
+
+Pause
+
+Get-Command -ListImported 
+
+Pause
---
+++ modules/logging/logging.psm1
@@ -0,0 +1,11 @@
+function Write-LogResult {
+param(
+    [string] $logpath,
+    [string] $logtype,
+    [string] $logdetail,
+    [srting] $errmsg
+    )
+
+
+}
+Export-ModuleMember -Function Write-LogResult
